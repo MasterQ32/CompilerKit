bd73d7b7d78415f09714a933899db6156895888d
Felix Queißner <felix@masterq32.de>
2018-05-04 18:40:58 +0200

    Initial release: Uncoupling from psi.

---
+++ Stubs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.CodeDom.Compiler
+{
+    public sealed class CompilerErrorCollection : List<CompilerError>
+    {
+        internal CompilerErrorCollection() => throw new NotSupportedException("CompilerErrorCollection is just stubbed.");
+    }
+
+    public sealed class CompilerError
+    {
+        internal CompilerError() => throw new NotSupportedException("CompilerErrorCollection is just stubbed.");
+
+        public string ErrorText { get; set; }
+
+        public bool IsWarning { get; set; }
+    }
+}
